3388f0b0133dd81c27bd183ce36e9808c6d9b3f2
Beatriz <103044603+beabritoliveira@users.noreply.github.com>
2024-06-14 23:20:43 -0300

    Delete Inteface directory

--- Inteface/my-app/services/request.ts
+++
@@ -1,9 +0,0 @@
-
-export function request<TResponse>(
-    url: string,
-    config: RequestInit = {}
-):Promise<TResponse> {
-    return fetch(url, config)
-        .then((response) => response.json())
-        .then((response) => response as TResponse);
-}
